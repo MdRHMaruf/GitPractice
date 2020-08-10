72a0df3fa3e215441d95327f5e43008db9142905
MdRHMaruf <mdrobiulhasan2622@gmail.com>
2020-08-11 00:40:51 +0600

    Second commit from author

--- GitPractice/src/com/git/MainClass.java
+++ GitPractice/src/com/git/MainClass.java
@@ -3,6 +3,8 @@ package com.git;
 public class MainClass {
 
 	public static void main(String args[]) {
+		//And May be it's conflict with the file
 		System.out.println("It's Enough");
+		System.out.println("Another Commit from Author");
 	}
 }
